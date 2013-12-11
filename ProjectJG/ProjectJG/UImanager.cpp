ede3a171fc63d4fc51905f4dc9cdfd53e9a57cba
helloheesu <heesu@nhnnext.org>
2013-12-11 02:37:30 -0800

    쓸데없는 코드 3줄 지움....

--- ProjectJG/ProjectJG/UImanager.cpp
+++ ProjectJG/ProjectJG/UImanager.cpp
@@ -32,16 +32,19 @@ UImanager::UImanager(void)
 	m_UINum = 0;		// UI °³¼ö Ä«¿îÆ®
 
 	// FPS
-	m_FPSLabel = NNLabel::Create( L"FPS : ", L"¸¼Àº °íµñ", 20.f );
+	swprintf_s( m_FPSBuffer, _countof(m_FPSBuffer), L"FPS : " );
+	m_FPSLabel = NNLabel::Create( m_FPSBuffer, L"¸¼Àº °íµñ", 20.f );
 	m_FPSLabel->SetPosition( 0.f, 0.f );
 	m_UIList[m_UINum++] = m_FPSLabel;
 
 	// cost
-	m_Player1CostLabel = NNLabel::Create( L"Player1's Cost : ", L"¸¼Àº °íµñ", 20.f );
+	swprintf_s( m_Player1CostBuffer, _countof(m_Player1CostBuffer), L"Player1's Cost : " );
+	m_Player1CostLabel = NNLabel::Create( m_Player1CostBuffer, L"¸¼Àº °íµñ", 20.f );
 	m_Player1CostLabel->SetPosition( 0.f, 700.f );
 	m_UIList[m_UINum++] = m_Player1CostLabel;
-	
-	m_Player2CostLabel = NNLabel::Create( L"Player2's Cost : ", L"¸¼Àº °íµñ", 20.f );
+
+	swprintf_s( m_Player2CostBuffer, _countof(m_Player2CostBuffer), L"Player2's Cost : " );
+	m_Player2CostLabel = NNLabel::Create( m_Player2CostBuffer, L"¸¼Àº °íµñ", 20.f );
 	m_Player2CostLabel->SetPosition( 0.f, 100.f );
 	m_UIList[m_UINum++] = m_Player2CostLabel;
 }
@@ -59,13 +62,10 @@ void UImanager::Update( float dTime, CMaincharacter* Player1, CMaincharacter* Pl
 {
 	// FPS
 	swprintf_s( m_FPSBuffer, _countof(m_FPSBuffer), L"FPS : %0.3f", NNApplication::GetInstance()->GetFPS() );
-	m_FPSLabel->SetString( m_FPSBuffer );
 
 	// cost
 	swprintf_s( m_Player1CostBuffer, _countof(m_Player1CostBuffer), L"Player1's Cost : %d", (int)(Player1->GetCost()) );
-	m_Player1CostLabel->SetString( m_Player1CostBuffer );
 	swprintf_s( m_Player2CostBuffer, _countof(m_Player2CostBuffer), L"Player2's Cost : %d", (int)(Player2->GetCost()) );
-	m_Player2CostLabel->SetString( m_Player2CostBuffer );
 }
 
 void UImanager::SetAllVisible( bool visible )
